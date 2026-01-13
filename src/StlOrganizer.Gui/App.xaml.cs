2a1504f43ed514cdb931ef286612d33121cbc614
Christopher Loggins <christopher.loggins@gmail.com>
2026-01-13 06:55:32 -0600

    Refactor: Remove redundant base class declarations

--- src/StlOrganizer.Gui/App.xaml.cs
+++ src/StlOrganizer.Gui/App.xaml.cs
@@ -13,7 +13,7 @@ namespace StlOrganizer.Gui;
 /// <summary>
 ///     Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App
 {
     private readonly ServiceProvider serviceProvider;
 
--- src/StlOrganizer.Gui/MainWindow.xaml.cs
+++ src/StlOrganizer.Gui/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ namespace StlOrganizer.Gui;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow
 {
     public MainWindow(MainViewModel viewModel)
     {
