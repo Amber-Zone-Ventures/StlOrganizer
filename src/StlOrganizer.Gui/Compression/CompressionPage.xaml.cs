d280cae925b43fbe91d6414d34ba972f7c7b7f3e
Chris <chris@amberzoneventures.com>
2026-02-02 17:35:16 -0600

    feat: Add Compression page UI and code-behind

---
+++ src/StlOrganizer.Gui/Compression/CompressionPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StlOrganizer.Gui.Compression;
+
+public partial class CompressionPage
+{
+    public CompressionPage(CompressionViewModel viewModel)
+    {
+        InitializeComponent();
+        DataContext = viewModel;
+    }
+}
